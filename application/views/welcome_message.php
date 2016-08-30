107a3554ee8e460a0b98d2e327c4944ee5bb815f
Luciano Matsuo <matsuoluciano@gmail.com>
2016-08-30 15:08:09 -0300

    archivos base codeigniter

---
+++ application/views/welcome_message.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Practicando con CodeIgniter</title>
+	<style media="screen">
+		h1,p{
+			font-family:Arial;
+		}
+	</style>
+</head>
+<body>
+<h1>Trabajando con CodeIgniter</h1>
+<div id="container">
+	<p>
+		Este es el primer texto creado con el framework CodeIgniter
+	</p>
+</div>
+
+</body>
+</html>
